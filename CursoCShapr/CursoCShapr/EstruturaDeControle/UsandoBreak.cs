864d8d1dcba9c5de0707c99c7fe19fd77f94d644
VincydeTarsis <vincydetarsis@gmail.com>
2022-11-20 17:37:36 -0300

    Fechando Controle

---
+++ CursoCShapr/CursoCShapr/EstruturaDeControle/UsandoBreak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturaDeControle {
+    class UsandoBreak {
+        public static void Executar() {
+
+            Random random = new Random();
+            int numero = random.Next(1, 51);
+
+            Console.WriteLine($"O número que queremos é {numero}");
+
+            for (int i = 1; i <= 50; i++) {
+                Console.Write($"{i} é o número que queremos?");
+                if (i == numero) {
+                    Console.WriteLine("Sim!");
+                    break;
+                } else {
+                    Console.WriteLine("Não!");
+                }
+            }
+
+            Console.WriteLine("Acabou!");
+        }
+    }
+}
--- CursoCShapr/CursoCShapr/Program.cs
+++ CursoCShapr/CursoCShapr/Program.cs
@@ -34,6 +34,9 @@ namespace CursoCSharp {
                 {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar},
                 {"Estrutura Do While - Estruturas de Controle", EstruturaDoWhile.Executar},
                 {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
+                {"Estrutura Foreach - Estruturas de Controle", EstruturaForEach.Executar},
+                {"Usando Break - Estruturas de Controle", UsandoBreak.Executar},
+                {"Usando Continue - Estruturas de Controle", UsandoContinue.Executar},
 
             });
 
