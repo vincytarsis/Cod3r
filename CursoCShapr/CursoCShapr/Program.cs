ccd3fe7087f650a9048a4313d1af907340117ba8
VincydeTarsis <vincydetarsis@gmail.com>
2022-11-24 06:39:26 -0300

    Props

--- CursoCShapr/CursoCShapr/Program.cs
+++ CursoCShapr/CursoCShapr/Program.cs
@@ -51,6 +51,7 @@ namespace CursoCSharp {
                 {"Params - Classes e Métodos", Params.Executar},
                 {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar},
                 {"Gat e Set - Classes e Métodos", GetSet.Executar},
+                {"Props - Classes e Métodos", Props.Executar},
                 
 
             });
