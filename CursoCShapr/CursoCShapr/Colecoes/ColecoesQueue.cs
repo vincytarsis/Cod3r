2cd2af1a9ce52aa94c4e86304c660abb6ce9dadd
Vincy de Tarsis <96235421+vincytarsis@users.noreply.github.com>
2022-11-30 13:05:55 -0300

    Create ColecoesQueue.cs

---
+++ CursoCShapr/CursoCShapr/Colecoes/ColecoesQueue.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Colletction.Generic;
+
+namespace CursoCSharp.Colecoes
+{
+    class ColecoesQueue
+    {
+        public static void Executar()
+        {
+            var fila = new Queue<string>();
+
+            fila.Enqueue("Fulano");
+            fila.Enqueue("Ciclano");
+            fila.Enqueue("Beltrano");
+
+            Console.WriteLine(fila.Peek());
+            Console.WriteLine(fila.Count);
+
+            Console.WriteLine(fila.Dequeue());
+            Console.WriteLine(fila.Count);
+
+            foreach(var pessoa in flia)
+            {
+                Console.WriteLine(pessoa);
+            }
+
+            var salada = new Queue();
+            salada.Enqueue(3);
+            salada.Enqueue("Item");
+            salada.Enqueue(true);
+            salada.Enqueue(3.14)
+        }
+    }
+}
