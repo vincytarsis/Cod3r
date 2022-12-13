50a2811742aa55a768374bb6bc27fc289957773f
Vincy de Tarsis <96235421+vincytarsis@users.noreply.github.com>
2022-12-13 13:11:09 -0300

    Create LendoArquivos.cs

---
+++ CursoCShapr/CursoCShapr/Api/LendoArquivos.cs
@@ -0,0 +1,34 @@
+using System;
+using Sysytem.IO;
+
+namespace CursoCSharp.Api
+{
+    class LendoArquivos
+    {
+        public static void Executar()
+        {
+            var path = @"~/lendo_arquivo.txt".ParseHome();
+
+            if (!File.Exists(path)) { 
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine("Produto;Preço;Qtde");
+                    sw.WriteLine("Caneta Bic;1.9;10");
+                    sw.WriteLine("Folha Sufite;10.3;5");
+                }
+            }
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    var texto = sr.ReadToEnd();
+                    Console.WriteLine(texto);
+                }
+            }catch(Exception ex )
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
