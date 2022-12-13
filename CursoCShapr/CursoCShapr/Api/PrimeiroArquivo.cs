eb1b645af8080927f55f6e8cba1374173e071ec8
Vincy de Tarsis <96235421+vincytarsis@users.noreply.github.com>
2022-12-13 12:55:40 -0300

    Pronto

--- CursoCShapr/CursoCShapr/Api/PrimeiroArquivo.cs
+++ CursoCShapr/CursoCShapr/Api/PrimeiroArquivo.cs
@@ -1,14 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-namespace CursoCSharp.Api {
-    class PrimeiroArquivo {
+namespace CursoCSharp.Api
+{
+    class PrimeiroArquivo
+    {
 
-        public static void Executar() {
+        public static void Executar()
+        {
+            var path = @"~/primeiro_arquivo.txt".ParseHome();
 
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Esse é");
+                    sw.WriteLine("o nosso ");
+                    sw.WriteLine("Primeiro");
+                    sw.WriteLine("arquivo");
+                }
+            }
+            using (StreamWriter sw = FIle.AppendText(path))
+            {
+                sw.WriteLine("");
+                sw.WriteLine("É possivel");
+                sw.WriteLine("adicionar");
+                sw.WriteLine("mais texto!");
+            }
         }
     }
+}
 }
